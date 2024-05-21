07a171a071f8d940e80ff7062da8bad0b286980e
shohaib1996 <khanshohaibhossain@gmail.com>
2024-05-22 04:00:35 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
+const app: Application = express();
+
+// parser
+
+app.use(express.json());
+app.use(cors());
+// application routes
+app.use("/api/v1/students", StudentRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Success" });
+});
+
+export default app;
